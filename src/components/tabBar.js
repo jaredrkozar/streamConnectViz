e49089781554bb3e6b7905ea3e292c97f53f2b35
Jared Kozar <jared.rkozar@gmail.com>
2023-02-21 23:15:12 -0500

    added custom color set

--- src/components/tabBar.js
+++ src/components/tabBar.js
@@ -3,18 +3,19 @@ import classNames from 'classnames';
 import {appList} from "./tabBarItems"
 import { ButtonCreator, LocationTable, Map, SearchLocations } from '../components/components'
 import { useState } from "react";
+import { Image } from "react-bootstrap-icons";
 
 export function TabBar() {
     return (
         <div className=''>
 
 <Tab.Group>
-        <Tab.List className="transition-width ease flex space-x-1 pointer-events-none relative h-14 flex rounded-xl bg-[#000921] p-2 w-fit">
+        <Tab.List className="transition-width ease flex space-x-1 pointer-events-none relative h-14 flex rounded-xl bg-navyBlue-900 p-2 w-fit">
         {
           appList.map((tab) => (
             <Tab key={tab.name} className={({ selected }) =>
             classNames('focus:outline-none transition-width duration-500 ease p-4 pointer-events-auto inline-flex items-center gap-x-4 group rounded-lg text-xl font-medium text-white overflow-hidden',
-            selected ? 'w-fit bg-blue-500' : "hover:bg-blue-500/75 hover:ease-out duration-500 w-12 pl-2")}
+            selected ? 'w-fit bg-navyBlue-600' : "hover:bg-navyBlue-600/75 hover:ease-out duration-500 w-12 pl-2")}
             >
               <div className='items-center'>{tab.icon}</div>
               <div className='whitespace-nowrap focus-within:w-40'>{tab.name}</div>
@@ -28,6 +29,12 @@ export function TabBar() {
         </Tab.Panels>
       </Tab.Group>
 
+            <div className='absolute top-0 right-0'>
+              <button className='pl-2 pr-2 h-14 bg-navyBlue-600 hover:bg-navyBlue-700 pointer-events-auto inline-flex items-center gap-x-4 group rounded-lg text-xl font-medium text-white overflow-hidden'>
+                  <Image></Image>
+                  <h5>Get Pictures</h5>
+              </button>
+            </div>
         </div>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'navyBlue': {
+          DEFAULT: '#00081F',
+          50: '#FFFFFF',
+          100: '#FFFFFF',
+          200: '#CCFDFF',
+          300: '#8FEFFF',
+          400: '#52D5FF',
+          500: '#14AFFF',
+          600: '#0078D6',
+          700: '#004799',
+          800: '#00225C',
+          900: '#00081F'
+        },
+      }
+    }
   },
   transitionProperty: {
     'width': 'width',
