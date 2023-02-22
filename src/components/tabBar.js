3915ef7d65d3fed761b5561bba9da143dfa86fed
Jared Kozar <jared.rkozar@gmail.com>
2023-02-21 20:02:59 -0500

    fixed tab bar transitions

--- src/components/tabBar.js
+++ src/components/tabBar.js
@@ -9,12 +9,12 @@ export function TabBar() {
         <div className=''>
 
 <Tab.Group>
-        <Tab.List className="flex space-x-1 pointer-events-none relative h-14 flex rounded-xl bg-[#000921] p-2 w-fit">
+        <Tab.List className="transition-width ease flex space-x-1 pointer-events-none relative h-14 flex rounded-xl bg-[#000921] p-2 w-fit">
         {
           appList.map((tab) => (
             <Tab key={tab.name} className={({ selected }) =>
-            classNames('p-4 pointer-events-auto inline-flex items-center gap-x-4 group rounded-lg text-xl font-medium text-white overflow-hidden',
-            selected ? 'bg-blue-500' : "hover:bg-blue-500/75 hover:animate-fade-in-down w-12 pl-2")}
+            classNames('focus:outline-none transition-width duration-500 ease p-4 pointer-events-auto inline-flex items-center gap-x-4 group rounded-lg text-xl font-medium text-white overflow-hidden',
+            selected ? 'w-fit bg-blue-500' : "hover:bg-blue-500/75 hover:ease-out duration-500 w-12 pl-2")}
             >
               <div className='items-center'>{tab.icon}</div>
               <div className='whitespace-nowrap focus-within:w-40'>{tab.name}</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-          'fade-in-down': {
-              '0%': {
-                  opacity: '0',
-              },
-              '100%': {
-                  opacity: '1',
-              },
-          }
-      },
-      animation: {
-          'fade-in-down': 'fade-in-down 0.2s ease-out'
-      }
-  },
+    extend: {}
   },
   transitionProperty: {
     'width': 'width',
