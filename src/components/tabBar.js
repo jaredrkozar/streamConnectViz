fd6a9d78e8cb77f585dd253ff714d70a3145844d
Jared Kozar <jared.rkozar@gmail.com>
2023-02-23 12:13:59 -0500

    light mode fixes

--- src/components/tabBar.js
+++ src/components/tabBar.js
@@ -13,12 +13,12 @@ export function TabBar() {
         <div className=''>
 
 <Tab.Group>
-        <Tab.List className="transition-width ease flex space-x-1 pointer-events-none relative h-14 flex rounded-xl bg-navyBlue-900 p-2 w-fit">
+        <Tab.List className="transition-width ease flex space-x-1 pointer-events-none relative h-14 flex rounded-xl bg-slate-400 dark:bg-navyBlue-900 p-2 w-fit">
         {
           appList.map((tab) => (
             <Tab key={tab.name} className={({ selected }) =>
             classNames('focus:outline-none transition-width duration-500 ease p-4 pointer-events-auto inline-flex items-center gap-x-4 group rounded-lg text-xl font-medium text-white overflow-hidden',
-            selected ? 'w-12 lg:w-fit bg-navyBlue-600' : "hover:bg-navyBlue-700 hover:ease-out duration-500 w-12 pl-2")}
+            selected ? 'w-12 lg:w-fit bg-navyBlue-500 dark:bg-navyBlue-600' : "dark:hover:bg-navyBlue-700 hover:bg-navyBlue-400/75 hover:ease-out duration-500 w-12 pl-2")}
             >
               <div className='items-center'>{tab.icon}</div>
               <div className='whitespace-nowrap focus-within:w-40'>{tab.name}</div>
@@ -33,7 +33,7 @@ export function TabBar() {
       </Tab.Group>
 
             <div className='absolute top-0 right-0'>
-              <button className='bg-navyBlue-600 hover:bg-navyBlue-700 hover:ease-out duration-500 w-20 lg:w-fit h-14 p-2 pointer-events-auto flex flex-row items-center gap-x-4 group rounded-lg text-xl font-medium text-white overflow-hidden' onClick={event => ImageButtonClicked(dispatch)}>
+              <button className='bg-navyBlue-500 hover:bg-navyBlue-600 dark:bg-navyBlue-600 dark:hover:bg-navyBlue-700 hover:ease-out duration-500 w-20 lg:w-fit h-14 p-2 pointer-events-auto flex flex-row items-center gap-x-4 group rounded-lg text-xl font-medium text-white overflow-hidden' onClick={event => ImageButtonClicked(dispatch)}>
                 <div className='items-center'>Get Images</div>
                 <Images size={26}></Images>
               </button>
